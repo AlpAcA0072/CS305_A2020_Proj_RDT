29bd68d8188a57824cf373a1be20e14a33780eeb
AlpAcA0072 <AlpAcA0072@gmail.com>
2020-12-25 13:46:50 +0800

    refresh of server.py and client.py

--- client.py
+++ client.py
@@ -1,27 +1,68 @@
-server_address2 = ('127.0.0.1', 8081)
-from USocket import UnreliableSocket
+# server_address2 = ('127.0.0.1', 8081)
+# from USocket import UnreliableSocket
+# from rdt import RDTSocket
+# import socket
+#
+# # s=socket.socket()
+# # s.connect()
+# if __name__ == '__main__':
+#     socket2 = RDTSocket()
+#     socket2.bind(('127.0.0.1', 8080))
+#     socket2.connect(server_address2)
+#     with open('alice.txt', 'r') as f:
+#         data = f.read()
+#     data = data.encode()
+#     socket2.send(data)
+#     # l = len(data)
+#     # print(l)
+#     # k = l / 1000
+#     # print(k)
+#     # for i in range(int(k + 1)):
+#     #     print(i)
+#     #     if i != k:
+#     #         socket2.send(data[i * 1000:(i + 1) * 1000])
+#     #         print(data[i * 1000:(i + 1) * 1000].decode())
+#     #     else:
+#     #         socket2.send(data[i * 1000:])
+#     #         print(data[i * 1000:].decode())
 from rdt import RDTSocket
-import socket
+import time
+from difflib import Differ
 
-# s=socket.socket()
-# s.connect()
-if __name__ == '__main__':
-    socket2 = RDTSocket()
-    socket2.bind(('127.0.0.1', 8080))
-    socket2.connect(server_address2)
-    with open('alice.txt', 'r') as f:
-        data = f.read()
-    data = data.encode()
-    socket2.send(data)
-    # l = len(data)
-    # print(l)
-    # k = l / 1000
-    # print(k)
-    # for i in range(int(k + 1)):
-    #     print(i)
-    #     if i != k:
-    #         socket2.send(data[i * 1000:(i + 1) * 1000])
-    #         print(data[i * 1000:(i + 1) * 1000].decode())
-    #     else:
-    #         socket2.send(data[i * 1000:])
-    #         print(data[i * 1000:].decode())
+client = RDTSocket()
+client.connect(('127.0.0.1', 9999))
+
+data_count = 0
+echo = b''
+count = 3
+
+with open('alice.txt', 'r') as f:
+    data = f.read()
+    encoded = data.encode()
+    assert len(data) == len(encoded)
+
+start = time.perf_counter()
+for i in range(count):  # send 'alice.txt' for count times
+    data_count += len(data)
+    client.send(encoded)
+
+'''
+blocking send works but takes more time 
+'''
+
+while True:
+    reply = client.recv(2048)
+    echo += reply
+    print(reply)
+    if len(echo) == len(encoded) * count:
+        break
+client.close()
+
+'''
+make sure the following is reachable
+'''
+
+print(f'transmitted {data_count}bytes in {time.perf_counter() - start}s')
+diff = Differ().compare(data.splitlines(keepends=True), echo.decode().splitlines(keepends=True))
+for line in diff:
+    assert line.startswith('  ')  # check if data is correctly echoed
--- server.py
+++ server.py
@@ -1,15 +1,36 @@
-server_address = ('127.0.0.1', 12345)
-server_address2 = ('127.0.0.1', 8081)
-from USocket import UnreliableSocket
+# server_address = ('127.0.0.1', 12345)
+# server_address2 = ('127.0.0.1', 8081)
+# from USocket import UnreliableSocket
+# from rdt import RDTSocket
+# import socket
+#
+# # s=socket.socket()
+# # s.connect()
+# if __name__ == '__main__':
+#     socket = RDTSocket()
+#     socket.bind(server_address2)
+#     socket.accept()
+#     while True:
+#         socket.recv(2048)
+#         # socket.recv(1446)
 from rdt import RDTSocket
-import socket
+import time
 
-# s=socket.socket()
-# s.connect()
 if __name__ == '__main__':
-    socket = RDTSocket()
-    socket.bind(server_address2)
-    socket.accept()
+    server = RDTSocket()
+    server.bind(('127.0.0.1', 9999))
+
     while True:
-        socket.recv(2048)
-        # socket.recv(1446)
+        conn, client_addr = server.accept()
+        start = time.perf_counter()
+        while True:
+            data = conn.recv(2048)
+            if data:
+                conn.send(data)
+            else:
+                break
+        '''
+        make sure the following is reachable
+        '''
+        conn.close()
+        print(f'connection finished in {time.perf_counter() - start}s')
